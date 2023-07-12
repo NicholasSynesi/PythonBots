8e3476e3b2ae86f5e2e3c6e575fde3abb9524ff1
NicholasSynesi <117459444+NicholasSynesi@users.noreply.github.com>
2023-07-12 18:41:12 +0100

    update

--- fbref_scraper.py
+++ fbref_scraper.py
@@ -7,13 +7,17 @@ league_list = []
 league_list.append(pd.read_html(url, index_col=False, flavor='lxml')[0])
 league_list = pd.concat(league_list, axis=0, ignore_index=True)
 
+# First, make an average of home xG vs away xG
+
 league_df = league_list[league_list['Wk'].notna()]
 
 league_df = league_df.rename(columns={'xG':'xGHome'
                    ,'xG.1':'xGAway'})
 
-league_df['HomeScore'] = league_df['Score'].str[0]
-league_df['AwayScore'] = league_df['Score'].str[2]
+league_df['HomeScore'] = pd.to_numeric(league_df['Score'].str[0], errors='coerce')
+league_df['AwayScore'] = pd.to_numeric(league_df['Score'].str[2], errors='coerce')
+#league_df['HomeScore'] = league_df['Score'].str[0]
+#league_df['AwayScore'] = league_df['Score'].str[2]
 
 league_df = league_df.drop(['Match Report', 'Notes'],axis=1)
 
@@ -28,11 +32,15 @@ league_df = league_df[['Wk','Day','Date','Time','Home',
 league_df = league_df.dropna()
 
 league_df = league_df[['Date','Home','HomeScore','xGHome',
-                       'AwayScore','xGAway','Away']].reset_index(drop=True)
+                       'AwayScore','xGAway','Away', 'Referee']].reset_index(drop=True)
 
 league_Mean_Home_xG = round((league_df['xGHome'].mean()),2)
 league_Mean_Away_xG = round((league_df['xGAway'].mean()),2)
 
+league_Avg_Home_Goals = round(league_df['HomeScore'].mean(), 2)
+league_Avg_Away_Goals = round(league_df['AwayScore'].mean(), 2)
+
+"""Note to self, average home xG is 1.58, away is 1.26"""
 
 
 
